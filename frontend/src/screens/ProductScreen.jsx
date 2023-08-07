e833ab16a7443a59b7624bdeabbc4f7b312b115c
Rathin R Nair <rathinrnair@gmail.com>
2023-08-07 10:57:31 +0530

    created express server & created routes to server products data from backend. installed nodemon & concurrently packages. installed dotenv package and created the .env file to hold the environment variables. updated HomeScreen & ProductSrceen to fetch product data from backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import { useParams, Link } from 'react-router-dom'
-import products from '../products'
+// import products from '../products'
 import { Row, Col, Card, Image, ListGroup, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 
 const ProductScreen = () => {
   const { id: productId } = useParams()
-  const product = products.find((p) => p._id === productId)
+  const [product, setProduct] = useState({})
+  useEffect(() => {
+    const fetchproduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`)
+      setProduct(data)
+    }
+    fetchproduct()
+  }, [productId])
 
   return (
     <>
@@ -18,7 +27,7 @@ const ProductScreen = () => {
           <Image src={product.image} alt={product.name} fluid></Image>
         </Col>
         <Col md={4}>
-          <ListGroup>
+          <ListGroup variant='flush'>
             <ListGroup.Item>
               <h3>{product.name}</h3>
             </ListGroup.Item>
@@ -29,16 +38,38 @@ const ProductScreen = () => {
               />
             </ListGroup.Item>
             <ListGroup.Item>Pricce : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              <p>{product.description}</p>
+            </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
           <Card>
             <ListGroup>
               <ListGroup.Item>
-                Price: <strong>${product.price}</strong>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                Status: {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
