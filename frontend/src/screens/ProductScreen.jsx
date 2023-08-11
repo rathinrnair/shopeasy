a2a75113cbe0f5ec8112f887ba70201a74bde78f
Rathin R Nair <rathinrnair@gmail.com>
2023-08-11 11:11:48 +0530

    installed reduxtoolkit and react redux to handle the application state. creted and configured the store.js file. created a rootApiSlice using create api & fetchbasequery to handle async request to bakend to fetch data. created productApiSlcie with product endpoints & injected to rootApiSlice. created components Loader & message Loader component to display the spinner element while fetching the data and a message component to diplay any errors or warning that can occur. also created a cartsliceReducer to handle cart functionality

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,80 +1,88 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
 import { useParams, Link } from 'react-router-dom'
-// import products from '../products'
 import { Row, Col, Card, Image, ListGroup, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
 
 const ProductScreen = () => {
   const { id: productId } = useParams()
-  const [product, setProduct] = useState({})
-  useEffect(() => {
-    const fetchproduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`)
-      setProduct(data)
-    }
-    fetchproduct()
-  }, [productId])
+
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsQuery(productId)
 
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid></Image>
-        </Col>
-        <Col md={4}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`from ${product.numReviews} Reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>Pricce : ${product.price}</ListGroup.Item>
-            <ListGroup.Item>
-              <p>{product.description}</p>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Button
-                  className='btn-block'
-                  type='button'
-                  disabled={product.countInStock === 0}
-                >
-                  Add To Cart
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <Row>
+            <Col md={5}>
+              <Image src={product.image} alt={product.name} fluid></Image>
+            </Col>
+            <Col md={4}>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <h3>{product.name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating
+                    value={product.rating}
+                    text={`from ${product.numReviews} Reviews`}
+                  />
+                </ListGroup.Item>
+                <ListGroup.Item>Pricce : ${product.price}</ListGroup.Item>
+                <ListGroup.Item>
+                  <p>{product.description}</p>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
+                <ListGroup>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>
+                        <strong>${product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button
+                      className='btn-block'
+                      type='button'
+                      disabled={product.countInStock === 0}
+                    >
+                      Add To Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </>
+      )}
     </>
   )
 }
