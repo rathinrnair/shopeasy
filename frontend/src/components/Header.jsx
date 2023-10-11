9c87fb0104ff32f9b69c6dbbb7e76bac81294d4f
Rathin R Nair <rathinrnair@gmail.com>
2023-10-11 22:53:28 +0530

    implimented frontend authentication & installed tostify package to show toast messages

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Badge, Navbar, Nav, Container } from 'react-bootstrap'
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { useSelector } from 'react-redux/es/hooks/useSelector'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
@@ -8,6 +8,12 @@ import React from 'react'
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart)
+  const { userInfo } = useSelector((state) => state.auth)
+
+  const logoutHandler = () => {
+    console.log('logout handler')
+  }
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -31,12 +37,25 @@ const Header = () => {
                   )}
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <FaUser />
-                  Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item href='#action/3.1'>
+                      Profile
+                    </NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <FaUser />
+                    Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import { Form, Button, Row, Col, FormGroup } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
+import { setCredentials } from '../slices/authSlice'
+import { useLoginMutation } from '../slices/usersApiSlice'
+import Loader from '../components/Loader'
+import { toast } from 'react-toastify'
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const submitHandler = (e) => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [login, { isLoading }] = useLoginMutation()
+
+  const { userInfo } = useSelector((state) => state.auth)
+
+  // extracting redirect from URL query params
+  const { search } = useLocation()
+  const sp = new URLSearchParams(search)
+  const redirect = sp.get('redirect') || '/'
+
+  // if user logged in navigate to redirect
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect)
+    }
+  }, [userInfo, redirect, navigate])
+
+  const submitHandler = async (e) => {
     e.preventDefault()
-    console.log('Submit clicked')
+    try {
+      const res = await login({ email, password }).unwrap()
+      dispatch(setCredentials({ ...res }))
+      navigate(redirect)
+    } catch (err) {
+      toast.error(err?.data?.message || err.error)
+    }
   }
 
   return (
@@ -35,13 +65,22 @@ const LoginScreen = () => {
           ></Form.Control>
         </FormGroup>
 
-        <Button type='submit' variant='primary' className='mt-2'>
+        <Button
+          type='submit'
+          variant='primary'
+          className='mt-2'
+          disabled={isLoading}
+        >
           Sign In
         </Button>
+        {isLoading && <Loader />}
       </Form>
       <Row className='py-3'>
         <Col>
-          New Customer? <Link to='/register'>Register Now</Link>
+          New Customer?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Register Now
+          </Link>
         </Col>
       </Row>
     </FormContainer>
